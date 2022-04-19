96284fde022da43cea2ce2fbfdd856a901f55122
Vinícius Lucas <viniciuslucas1564@gmail.com>
2022-04-19 16:10:44 -0300

    Added jest and created test folder

---
+++ tests/students-service.test.ts
@@ -0,0 +1,13 @@
+describe('students service should', () => {
+    describe('succeed on', () => {
+        test('create a student', () => {
+
+        })
+    })
+
+    describe('fail on', () => {
+        test('create a student', () => {
+
+        })
+    })
+})
