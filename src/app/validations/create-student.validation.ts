bbe493550abad52dab90317f19727dafd920d08d
Vinícius Lucas <viniciuslucas1564@gmail.com>
2022-04-19 12:35:28 -0300

    Create student validation added

---
+++ src/app/validations/create-student.validation.ts
@@ -0,0 +1,15 @@
+import { NextFunction, Request, Response } from "express";
+import { ICreateStudentDto } from "../dto/students.dto";
+import { validateName } from "../helpers/validate-name.helper";
+
+export function createStudentValidation(req: Request, res: Response, next: NextFunction) {
+    const name = validateName(req.body.name)
+
+    const body: ICreateStudentDto = {
+        name
+    }
+
+    req.body = body
+
+    next()
+}
