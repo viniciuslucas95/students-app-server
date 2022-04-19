6d22126b790ef5c39ed33118a171d5a79d826bf2
Vinícius Lucas <viniciuslucas1564@gmail.com>
2022-04-19 13:31:28 -0300

    Added status code constants and updated it on errors files

---
+++ src/app/constants/status-code.constant.ts
@@ -0,0 +1,7 @@
+export const STATUS_CODE = {
+    OK: 200,
+    CREATED: 201,
+    NO_CONTENT: 204,
+    BAD_REQUEST: 400,
+    NOT_FOUND: 404
+}
--- src/app/errors/bad-request-exception.error.ts
+++ src/app/errors/bad-request-exception.error.ts
@@ -1,9 +1,8 @@
+import { STATUS_CODE } from "../constants/status-code.constant"
 import { HttpRequestException } from "./http-request-exception.error"
-
-const BAD_REQUEST_STATUS_CODE = 400
 
 export class BadRequestException extends HttpRequestException {
     constructor(name: string, message?: string) {
-        super(name, BAD_REQUEST_STATUS_CODE, message)
+        super(name, STATUS_CODE.BAD_REQUEST, message)
     }
 }
