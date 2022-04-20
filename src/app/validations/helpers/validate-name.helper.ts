8c80d5364f0432946f1ebefb3ff197b3b72baecc
Vinícius Lucas <viniciuslucas1564@gmail.com>
2022-04-19 23:28:23 -0300

    Modified name validation

--- src/app/validations/helpers/validate-name.helper.ts
+++ src/app/validations/helpers/validate-name.helper.ts
@@ -1,19 +1,15 @@
 import { BadRequestException } from "../../errors/base/bad-request-exception.error"
 
-const MIN_NAME_LENGTH = 3
-const PORTUGUESE_PATTERN = /[a-zA-ZáÁéÉíÍóÓúÚãÃõÕâÂêÊôÔàÀèÈìÌòÒùÙüçÇ ']/gm
+const MIN_NAME_LENGTH = 2
+const PORTUGUESE_PATTERN = /[^a-zA-ZáÁéÉíÍóÓúÚãÃõÕâÂêÊôÔàÀèÈìÌòÒùÙüçÇ]/gm
 
 export function validateName(name: string) {
     if (!name) throw new BadRequestException('NameCannotBeNull')
     if (typeof name !== 'string') throw new BadRequestException('NameMustBeAString')
 
-    const notAllowedLetters = name.replace(PORTUGUESE_PATTERN, '')
+    const minNecessaryLetters = name.replace(PORTUGUESE_PATTERN, '')
 
-    if (notAllowedLetters.length > 0) throw new BadRequestException('InvalidNameLetter')
-
-    const normalLetters = name.replace(/[^a-zA-Z]/gm, '')
-
-    if (normalLetters.length < MIN_NAME_LENGTH) throw new BadRequestException('NameTooShort', 'It must have at least 3 letters')
+    if (minNecessaryLetters.length < MIN_NAME_LENGTH) throw new BadRequestException('NameTooShort', 'It must have at least 2 letters')
 
     name = name.replace(/'*$/gm, "")
     name = name.replace(/\s*$/gm, "")
