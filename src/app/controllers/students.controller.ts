7cb26f322f25370e93371576a4b80e2c02a971cd
Vinícius Lucas <viniciuslucas1564@gmail.com>
2022-04-20 14:05:59 -0300

    Removed try/catch from students controller

--- src/app/controllers/students.controller.ts
+++ src/app/controllers/students.controller.ts
@@ -15,56 +15,36 @@ export class StudentsController {
 
     private setupPost() {
         this.router.post('/', createStudentValidation, async (req: Request, res: Response, next: NextFunction) => {
-            try {
-                const result = await this.studentsService.createAsync(req.body)
-                res.status(STATUS_CODE.CREATED).json(result)
-            } catch (err) {
-                next(err)
-            }
+            const result = await this.studentsService.createAsync(req.body)
+            res.status(STATUS_CODE.CREATED).json(result)
         })
     }
 
     private setupPatch() {
         this.router.patch('/:id', updateStudentValidation, async (req: Request, res: Response, next: NextFunction) => {
-            try {
-                await this.studentsService.updateAsync(req.params.id, req.body)
-                res.sendStatus(STATUS_CODE.NO_CONTENT)
-            } catch (err) {
-                next(err)
-            }
+            await this.studentsService.updateAsync(req.params.id, req.body)
+            res.sendStatus(STATUS_CODE.NO_CONTENT)
         })
     }
 
     private setupDelete() {
         this.router.delete('/:id', async (req: Request, res: Response, next: NextFunction) => {
-            try {
-                await this.studentsService.deleteAsync(req.params.id)
-                res.sendStatus(STATUS_CODE.NO_CONTENT)
-            } catch (err) {
-                next(err)
-            }
+            await this.studentsService.deleteAsync(req.params.id)
+            res.sendStatus(STATUS_CODE.NO_CONTENT)
         })
     }
 
     private setupGetAll() {
         this.router.get('/', async (req: Request, res: Response, next: NextFunction) => {
-            try {
-                const result = await this.studentsService.getAllAsync()
-                res.json(result)
-            } catch (err) {
-                next(err)
-            }
+            const result = await this.studentsService.getAllAsync()
+            res.json(result)
         })
     }
 
     private setupGetOne() {
         this.router.get('/:id', async (req: Request, res: Response, next: NextFunction) => {
-            try {
-                const result = await this.studentsService.getOneAsync(req.params.id)
-                res.json(result)
-            } catch (err) {
-                next(err)
-            }
+            const result = await this.studentsService.getOneAsync(req.params.id)
+            res.json(result)
         })
     }
 }
